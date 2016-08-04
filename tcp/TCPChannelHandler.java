d6a1fa156568de1877049b9260c49f09fd31b94b
wu <312770787@qq.com>
2016-08-04 16:02:55 +0800

    Update TCPChannelHandler.java

--- tcp/TCPChannelHandler.java
+++ tcp/TCPChannelHandler.java
@@ -17,13 +17,23 @@ public class TCPChannelHandler extends ChannelInitializer<SocketChannel> {
         System.out.println("IP:" + e.localAddress().getHostName());
         System.out.println("Port:" + e.localAddress().getPort());
         System.out.println("报告完毕");
+        //全算
         e.pipeline().addLast(new IdleStateHandler(0, 0, 2));
+        //读超时
+        //   e.pipeline().addLast(new ReadTimeoutHandler( 2));
+        //写超时
+            // e.pipeline().addLast(new WriteTimeoutHandler( 2));
+            //----上面的3个是用于设计心跳
+           
         // 解码器
         // 基于换行符号
         e.pipeline().addLast(new LineBasedFrameDecoder(1024));
-
+        //---自定义分隔符---
+        byte[] bts = {-1};
+        e.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, Unpooled.copiedBuffer(bts)));
+        //----
         // 基于指定字符串【换行符，这样功能等同于LineBasedFrameDecoder】
-        e.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, false, Unpooled.wrappedBuffer(new byte[]{-1})));
+        // e.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, false, Unpooled.wrappedBuffer(new byte[]{-1})));
         // 基于最大长度
 //		e.pipeline().addLast(new FixedLengthFrameDecoder(4));
         // 解码转Sring
